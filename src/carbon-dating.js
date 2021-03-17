95f7f518619c2be15f3aecf10dc217a444a0e2dd
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-03-17 13:59:17 +0300

    refactor: update Math.log()

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -13,7 +13,7 @@ module.exports = function dateSample(sampleActivity) {
   const FINAL_ACTIVITY = Number(sampleActivity);
   const k = 0.693 / HALF_LIFE_PERIOD;
   
-  let t = (Math.log(MODERN_ACTIVITY / FINAL_ACTIVITY) / (Math.log(10))) / k;
+  let t = (Math.log(MODERN_ACTIVITY / FINAL_ACTIVITY)) / k;
 
   if(t <= 0 || t == NaN)
   {
