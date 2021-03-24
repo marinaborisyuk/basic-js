5a2db3d85be63d5ed5095c1a5f1c62aa3cfdde06
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-03-24 09:25:49 +0300

    feat: add recursive-depth

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(/*arr*/) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  for(let i = 0; i < arr.length; i++)
+  {
+    if(arr[i] == '--discard-next')
+    {
+      if(arr[i+1] == undefined)
+        arr.splice(i--);
+      else
+        arr.splice(i, 2);  
+    }
+    if(arr[i] == '--discard-prev')
+    {
+      if(arr[i-1] == undefined)
+        arr.splice(i--);
+      else
+        arr.splice(i-1, 2);  
+    }
+    if(arr[i] == '--double-next')
+    {
+      if(arr[i+1] == undefined)
+        arr.splice(i--);
+      else
+        arr[i] = arr[i+1]; 
+    }
+    if(arr[i] == '--double-prev')
+    {
+      if(arr[i-1] == undefined)
+        arr.splice(i--);
+      else
+        arr[i] = arr[i-1]; 
+    }
+  }
+
+  return arr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(/*date*/) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+  // if(!data || data.getFullYear() <= '1970')
+  //   return 'Unable to determine the time of year!';
+
+  // let newDate = 
+
+  // if((data.getMonth() >= 0 && data.getMonth() <=1) || data.getMonth() == 11)
+  //   return 'winter';
+  // if(data.getMonth() >= 2 && data.getMonth() <=4)
+  //   return 'spring';
+
+  //   if(data.getMonth() >= 5 && data.getMonth() <=7)
+  //   return 'summer';  
+
+  //   if(data.getMonth() >= 8 && data.getMonth() <=10)
+  //   return 'autumn';
 };
